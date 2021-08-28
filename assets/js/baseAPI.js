38bdeb378162e9fef41c48e76e98d1976eccf15f
yusu <1071800463@qq.com>
2021-08-28 11:06:04 +0800

    完成了主页的开发，并修改了一些错误

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,3 +1,25 @@
 $.ajaxPrefilter(function (options) {
+    // 统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+
+    // 统一为有权限的接口，设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    // 全局统一挂载 complete 回调函数
+    // 身份验证，防止直接访问后台
+    // 在 complete 回调函数中，使用 res.responseJSON 得到响应回来的数据
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+        }
+    }
+
+
+
+
 })
---
+++ assets/js/index.js
@@ -0,0 +1,59 @@
+$(function () {
+
+    getUserInfo();
+
+    // 用户退出
+    $('#btnLogout').on('click', function () {
+        layui.layer.confirm('是否退出登录？', { icon: 3, title: '提示' }, function (index) {
+            localStorage.removeItem('token');
+            location.href = '/login.html';
+            // layui方法 关闭询问框
+            layer.close(index);
+        });
+    })
+
+
+
+})
+
+// 获取用户基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败！')
+            }
+
+            renderAvatar(res.data);
+        }
+
+    })
+}
+
+// 渲染用户头像
+function renderAvatar(user) {
+    let name = user.nickname || user.username;
+    let time = getTime();
+    $('#welcome').html(time + '&nbsp;&nbsp' + name);
+    $('.userinfo').show();
+    if (user.user_pic !== null) {
+        $('.text-avatar').hide();
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+    } else {
+        $('.layui-nav-img').hide();
+        let first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
+}
+
+// 时间
+function getTime() {
+    let time = new Date();
+    let h = time.getHours();
+    return h > 19 ? '晚上好' : h > 13 ? '下午好' : h > 11 ? '中午好' : h > 8 ? '上午好' : h > 4 ? '早上好' : '晚上好'
+}
